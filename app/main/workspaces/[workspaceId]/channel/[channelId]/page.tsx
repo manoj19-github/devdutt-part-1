1277b72c65c2612bd0030ff1739bcaba889c9b04
Manoj Santra <santramanoj1997@gmail.com>
2024-11-01 11:17:10 +0530

    working on channels

---
+++ app/main/workspaces/[workspaceId]/channel/[channelId]/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+type ChannelIdPageProps = {};
+
+const ChannelIdPage: FC<ChannelIdPageProps> = (): JSX.Element => {
+  return <div>channelIdPage</div>;
+};
+
+export default ChannelIdPage;
--- app/main/workspaces/[workspaceId]/page.tsx
+++ app/main/workspaces/[workspaceId]/page.tsx
@@ -1,14 +1,62 @@
 "use client";
+import useGetChannels from "@/hooks/useGetChannels";
 import { useGetWorkSpaceById } from "@/hooks/useGetWorkSpace";
+import useIsMounted from "@/hooks/useIsMounted";
 import { useWorkspaceId } from "@/hooks/useWorkspaceId";
-import React, { FC } from "react";
+import { useCreateChannelModal } from "@/stores/useCreateChannelModal";
+import { Loader, Triangle, TriangleAlert } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { FC, Fragment, useEffect, useMemo } from "react";
 
 type WorkspaceIdPageProps = {};
-const WorkspaceIdPage: FC<WorkspaceIdPageProps> = () => {
+const WorkspaceIdPage: FC<WorkspaceIdPageProps> = (): JSX.Element => {
   const workSpaceId = useWorkspaceId();
+  const router = useRouter();
+  const isMounted = useIsMounted();
   const { data } = useGetWorkSpaceById({ id: workSpaceId });
-  console.log("data: ", data);
-  return <div>WorkspaceIdPage : {workSpaceId}</div>;
+  const getWorkspaceByResponse = useGetWorkSpaceById({ id: workSpaceId });
+  const getChannelResponse = useGetChannels({ workspaceId: workSpaceId });
+  const [openCreateChannelModal, setOpenCreateChannelModal] =
+    useCreateChannelModal();
+  const channelId = useMemo(
+    () => getChannelResponse.response?.data?.[0]?._id,
+    [getChannelResponse.response?.data]
+  );
+  useEffect(() => {
+    if (getWorkspaceByResponse.isLoading || getChannelResponse.isLoading)
+      return;
+    if (channelId)
+      return router.push(
+        `/main/workspaces/${workSpaceId}/channel/${channelId}`
+      );
+    else if (!openCreateChannelModal) setOpenCreateChannelModal(true);
+  }, [
+    channelId,
+    getChannelResponse.isLoading,
+    getWorkspaceByResponse.isLoading,
+    getWorkspaceByResponse.data,
+    openCreateChannelModal,
+    setOpenCreateChannelModal,
+    router,
+    workSpaceId,
+  ]);
+  if (!isMounted) return <></>;
+  if (getChannelResponse.isLoading || getWorkspaceByResponse.isLoading)
+    return (
+      <div className="h-full flex-1 items-center justify-center flex-col gap-y-2">
+        <Loader className="h-8 w-8 animate-spin text-muted-foreground" />
+      </div>
+    );
+  if (!getWorkspaceByResponse.data)
+    return (
+      <div className="h-full flex-1 items-center justify-center flex-col gap-y-2">
+        <TriangleAlert className="h-8 w-8  text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Workspace Not Found
+        </span>
+      </div>
+    );
+  return <Fragment></Fragment>;
 };
 
 export default WorkspaceIdPage;
